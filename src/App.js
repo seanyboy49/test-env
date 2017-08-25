e66e972ada1ef3a812c07541ea0e30db6cf8c167
Sean Lee <seanboramlee@gmail.com>
2017-08-24 17:29:37 -0700

    add react backend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-// import Banner160x600 from './components/Banner160x600';
+import Banner160x600 from './components/Banner160x600';
 import Banner300x600 from './components/Banner300x600';
-// import Banner250x250 from './components/Banner250x250';
+import Banner250x250 from './components/Banner250x250';
 import DynamicInput from './containers/DynamicInput';
 
 
 class App extends Component {
-
   render() {
     return (
       <div className="App">
